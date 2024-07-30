310ab6d28df23ee7335b38f8b3c3f10f658bfc22
savanist <uyggyutttrt@gmail.com>
2024-07-30 18:38:23 +0100

    Update config.lua

--- mta-meta-discord-log/config.lua
+++ mta-meta-discord-log/config.lua
@@ -2,7 +2,7 @@ Config = {}
 Config.discord_logs = {
     ---example----
     ["lua-tr"] = { --this block add log details
-        webhook = '',
+        webhook = 'https://discord.com/api/webhooks/1267896280114532393/ktwOgyW6Wg0qKyCjUXE9eXol_rjfAm0_hg40pDugLCV35DzxzLQXujBt_0AxcdrN6J5i',
         image = 'https://cdn.discordapp.com/attachments/941753982169251901/1058085996907991050/png_logo.png',
         color = 7419530,
     },
@@ -13,7 +13,7 @@ Config.discord_logs = {
 -- DON'T EDIT ( SAKIN DÜZENLEME)
 ----------------------------------
 Config.standart_log = {
-    webhook ="",
+    webhook ="https://discord.com/api/webhooks/1267898997973192824/qIKWcxd_Y7F5oJ2bIDUivm7gajKDnNaM978pYP8xGpvK59vTSnRI-f_UjG1LKD9GuL_Y",
     image = "https://cdn.discordapp.com/attachments/941753982169251901/1058085996907991050/png_logo.png",
     color = 7419530,
 }
